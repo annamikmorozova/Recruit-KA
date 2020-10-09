57eca75d80da73f4921d5ea8e83120f695e31778
Anna Morozova <anna.mik.morozova@gmail.com>
2020-10-09 01:47:46 -0400

    Feat: add fake users and admin

---
+++ script/seedUsers.js
@@ -0,0 +1,38 @@
+const admins = [
+	{
+		firstName: "admin",
+		lastName: "admin",
+		email: "admin@gmail.com",
+		password: "admin",
+		role: "admin"
+	}
+];
+
+const users = [
+	{
+		firstName: "Erik",
+		lastName: "Y",
+		email: "erik@gmail.com",
+		password: "12345",
+		role: "player"
+	},
+	{
+		firstName: "Anna",
+		lastName: "M",
+		email: "anna@gmail.com",
+		password: "12345",
+		role: "player"
+	},
+	{
+		firstName: "Alisa",
+		lastName: "M",
+		email: "alisa@gmail.com",
+		password: "12345",
+		role: "player"
+	}
+];
+
+module.exports = {
+	admins,
+	users
+};
